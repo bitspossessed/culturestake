6f4d26c6d622d7efa3fd5f816cb56c1045f87027
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-02-20 15:14:08 +0100

    Use react-form, Joi for validation and api request state in forms

--- src/client/views/AdminLogin.js
+++ src/client/views/AdminLogin.js
@@ -1,32 +1,95 @@
-import React, { Fragment, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import Joi from '@hapi/joi';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm, useField, splitFormProps } from 'react-form';
 
 import Footer from '~/client/components/Footer';
 import Header from '~/client/components/Header';
 import View from '~/client/components/View';
-import { requestToken } from '~/client/store/app/actions';
+import { requestToken, TOKEN_REQUEST_ID } from '~/client/store/app/actions';
+
+const validationOptions = {
+  errors: {
+    label: false,
+  },
+};
+
+// eslint-disable-next-line react/display-name
+const InputField = React.forwardRef((props, ref) => {
+  const [field, fieldOptions, rest] = splitFormProps(props);
+
+  const {
+    meta: { error, isTouched, isValidating },
+    getInputProps,
+  } = useField(
+    field,
+    Object.assign({}, fieldOptions, {
+      defaultValue: fieldOptions.defaultValue || '',
+      validate: value => {
+        const { error } = schema[field].validate(value, validationOptions);
+
+        if (error) {
+          return error.message;
+        }
+
+        return false;
+      },
+    }),
+  );
+
+  // Build the field
+  return (
+    <Fragment>
+      <input {...getInputProps({ ref, ...rest })} />{' '}
+      {isValidating ? (
+        <em>Validating...</em>
+      ) : isTouched && error ? (
+        <em>{error}</em>
+      ) : null}
+    </Fragment>
+  );
+});
+
+const schema = {
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+};
 
 const AdminLogin = () => {
   const dispatch = useDispatch();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const {
+    isError = false,
+    isSuccess = false,
+    isPending = false,
+    error,
+  } = useSelector(state => {
+    return state.api.requests[TOKEN_REQUEST_ID] || {};
+  });
 
-  const onClick = event => {
-    event.preventDefault();
-
-    dispatch(requestToken(email, password));
-  };
-
-  const onChange = event => {
-    const { name, value } = event.target;
-
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'password') {
-      setPassword(value);
+  useEffect(() => {
+    if (isError) {
+      window.alert(error.message);
+    } else if (isSuccess) {
+      window.alert('Success!');
     }
-  };
+  }, [isError, isSuccess]);
+
+  const {
+    Form,
+    meta: { canSubmit },
+  } = useForm({
+    onSubmit: async values => {
+      const { value: sanitizedValues } = Joi.object(schema).validate(values);
+      const { email, password } = sanitizedValues;
+
+      dispatch(requestToken(email, password));
+    },
+  });
+
+  const isDisabled = !canSubmit || isPending;
 
   return (
     <Fragment>
@@ -35,35 +98,14 @@ const AdminLogin = () => {
       <View>
         <h1>Login</h1>
 
-        <form onSubmit={onClick}>
-          <fieldset>
-            <label htmlFor="email">Email</label>
+        <Form>
+          <InputField field="email" type="email" />
+          <InputField field="password" type="password" />
 
-            <input
-              id="email"
-              name="email"
-              type="email"
-              value={email}
-              onChange={onChange}
-            />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="password">Password</label>
-
-            <input
-              id="password"
-              name="password"
-              type="password"
-              value={password}
-              onChange={onChange}
-            />
-          </fieldset>
-
-          <button type="submit" onClick={onClick}>
+          <button disabled={isDisabled} type="submit">
             Login
           </button>
-        </form>
+        </Form>
       </View>
 
       <Footer />
