42635fd5211d29f358fb0b7c1eeaba99a31d5479
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-04-15 19:59:18 +0200

    Animate icons automatically, sometimes

--- src/client/components/ThreeModelAnimated.js
+++ src/client/components/ThreeModelAnimated.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import React, { Suspense, useRef, useEffect, useState } from 'react';
-import { Group, BoxBufferGeometry, Mesh } from 'react-three-fiber/components';
+import { Group, BoxGeometry, Mesh } from 'react-three-fiber/components';
 import { Vector3 } from 'three';
 import { useFrame } from 'react-three-fiber';
 
 import ThreeModel from '~/client/components/ThreeModel';
 import ThreeSpinner from '~/client/components/ThreeSpinner';
-import { randomFromArray } from '~/common/utils/random';
+import { randomRange, randomFromArray } from '~/common/utils/random';
+
+const RANDOM_ANIMATION_MIN = 8000;
+const RANDOM_ANIMATION_MAX = 25000;
 
 const eulerVector = new Vector3(Math.PI * 2, Math.PI * 2, Math.PI * 2);
 
@@ -16,22 +19,57 @@ const possibleRotations = [
   new Vector3(0, 0, 1),
 ];
 
-const ThreeModelAnimated = (props) => {
+const ThreeModelAnimated = ({ onClick, ...props }) => {
+  let timeout;
+
   const model = useRef();
 
   const [originalRotation, setOriginalRotation] = useState(new Vector3());
   const [targetRotation, setTargetRotation] = useState(new Vector3());
+  const [isHovered, setIsHovered] = useState(false);
+
+  // Use references to keep state inside of setTimeout
+  // https://github.com/facebook/react/issues/14010
+  const originalRotationRef = useRef(originalRotation);
+  originalRotationRef.current = originalRotation;
+
+  const targetRotationRef = useRef(targetRotation);
+  targetRotationRef.current = targetRotation;
+
+  const isHoveredRef = useRef(isHovered);
+  isHoveredRef.current = isHovered;
 
   const animate = () => {
     const target = eulerVector
       .clone()
       .multiply(randomFromArray(possibleRotations));
 
-    setTargetRotation(originalRotation.add(target));
+    setTargetRotation(originalRotationRef.current.add(target));
   };
 
   const onPointerEnter = () => {
+    setIsHovered(true);
     animate();
+  };
+
+  const onPointerLeave = () => {
+    setIsHovered(false);
+  };
+
+  const nextRandomAnimation = () => {
+    clearRandomAnimation();
+
+    timeout = window.setTimeout(() => {
+      if (!isHoveredRef.current) {
+        animate();
+      }
+
+      nextRandomAnimation();
+    }, randomRange(RANDOM_ANIMATION_MIN, RANDOM_ANIMATION_MAX));
+  };
+
+  const clearRandomAnimation = () => {
+    window.clearTimeout(timeout);
   };
 
   useFrame((state, delta) => {
@@ -40,7 +78,10 @@ const ThreeModelAnimated = (props) => {
     }
 
     const { rotation } = model.current;
-    rotation.setFromVector3(rotation.toVector3().lerp(targetRotation, delta));
+
+    rotation.setFromVector3(
+      rotation.toVector3().lerp(targetRotationRef.current, delta),
+    );
   });
 
   useEffect(() => {
@@ -48,6 +89,12 @@ const ThreeModelAnimated = (props) => {
 
     setOriginalRotation(initialRotation);
     setTargetRotation(initialRotation);
+
+    nextRandomAnimation();
+
+    return () => {
+      clearRandomAnimation();
+    };
   }, [props.rotation]);
 
   return (
@@ -55,11 +102,12 @@ const ThreeModelAnimated = (props) => {
       <Group
         {...props}
         ref={model}
-        onClick={props.onClick}
+        onClick={onClick}
         onPointerEnter={onPointerEnter}
+        onPointerLeave={onPointerLeave}
       >
         <Mesh visible={false}>
-          <BoxBufferGeometry args={[15, 15, 15]} attach="geometry" />
+          <BoxGeometry args={[10, 10, 10]} attach="geometry" />
         </Mesh>
 
         <ThreeModel url={props.url} />
