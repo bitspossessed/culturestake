d8a4b5f988ba1e74655ecadb9ad848693d2429d9
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-05-06 18:22:45 +0200

    Add a note to not create Votes in the database when dispatching failed

--- src/server/controllers/votes.js
+++ src/server/controllers/votes.js
@@ -1,12 +1,15 @@
-import { respondWithSuccess } from '~/server/helpers/respond';
-import dispatch from '~/server/services/dispatcher';
 import Vote from '~/server/models/vote';
+import dispatch from '~/server/services/dispatcher';
+import { respondWithSuccess } from '~/server/helpers/respond';
 
 async function create(req, res, next) {
   const vote = req.body;
+
   try {
+    // @TODO: Revert vote creation when dispatch failed
     await Vote.create(vote);
     await dispatch(vote);
+
     respondWithSuccess(res);
   } catch (error) {
     return next(error);
