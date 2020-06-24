035e26c3e1aeb263eaf79a9fd2fd7fb2957a6d5f
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-06-24 22:15:55 +0200

    Fix broken hook causing infinite loop when uploading files

--- src/client/hooks/requests.js
+++ src/client/hooks/requests.js
@@ -26,7 +26,7 @@ export const useRequest = (requestId, { onError, onSuccess }) => {
     } else if (isSuccess && onSuccess) {
       onSuccess(response);
     }
-  }, [isError, isSuccess, onError, onSuccess, error, response]);
+  }, [isError, isSuccess]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return {
     isError,
