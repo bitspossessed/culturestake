dd90e822b7d9fed25b32c8313534ec9fa556b227
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-10 22:32:23 +0200

    Pass on previous owner to change owners list in contracts

--- src/common/services/contracts/owners.js
+++ src/common/services/contracts/owners.js
@@ -1,26 +1,47 @@
+import { SENTINEL_ADDRESS } from '~/common/utils/constants';
 import { adminContract } from '~/common/services/contracts';
 
 export const TX_ADDED_OWNER = Symbol('TX_ADDED_OWNER');
 export const TX_REMOVED_OWNER = Symbol('TX_REMOVED_OWNER');
 
-export async function isOwner(ownerAddress) {
+async function isOwner(ownerAddress) {
   return adminContract.methods.isOwner(ownerAddress).call();
 }
 
-export async function getOwners(ownerAddress) {
-  return adminContract.methods.getOwners(ownerAddress).call();
+async function getOwners() {
+  return adminContract.methods.getOwners().call();
 }
 
-export async function addOwner(sender, ownerAddress) {
-  const txHash = await adminContract.methods
+async function addOwner(senderAddress, ownerAddress) {
+  const { transactionHash: txHash } = await adminContract.methods
     .addOwner(ownerAddress)
-    .send({ from: sender });
+    .send({
+      from: senderAddress,
+    });
+
   return { txHash, txMethod: TX_ADDED_OWNER };
 }
 
-export async function removeOwner(sender, ownerAddress) {
-  const txHash = await adminContract.methods
-    .removeOwner(ownerAddress)
-    .send({ from: sender });
+async function removeOwner(senderAddress, ownerAddress) {
+  const owners = await getOwners();
+
+  // .. to find out which previous owner in the list is pointing at the one we
+  // want to remove
+  const ownerIndex = owners.findIndex((owner) => owner === ownerAddress);
+  const prevOwner = ownerIndex > 0 ? owners[ownerIndex - 1] : SENTINEL_ADDRESS;
+
+  const { transactionHash: txHash } = await adminContract.methods
+    .removeOwner(prevOwner, ownerAddress)
+    .send({
+      from: senderAddress,
+    });
+
   return { txHash, txMethod: TX_REMOVED_OWNER };
 }
+
+export default {
+  addOwner,
+  getOwners,
+  isOwner,
+  removeOwner,
+};
