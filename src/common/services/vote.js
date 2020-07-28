8e931a35511e4b0faf5586a70565cc3c6b5a6d91
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-28 15:04:13 +0200

    Improve vote encoding

--- src/common/services/vote.js
+++ src/common/services/vote.js
@@ -1,8 +1,9 @@
 import web3 from '~/common/services/web3';
 import { packVote, packBooth } from '~/common/services/encoding';
 
-const VOTE_DATA_FESTIVAL_TOKEN = '|';
-const VOTE_DATA_SEPARATOR = '/';
+const VOTE_DATA_FESTIVAL_TOKEN = '-';
+const VOTE_DATA_SEPARATOR = '|';
+const VOTE_DATA_SIGNATURE_TOKEN = 'e1ec7';
 
 export function signBooth({ festivalAnswerIds, privateKey, nonce }) {
   return web3.eth.accounts.sign(packBooth(festivalAnswerIds, nonce), privateKey)
@@ -34,23 +35,49 @@ export function encodeVoteData({
   signature,
 }) {
   return [
-    ...festivalAnswerIds,
-    VOTE_DATA_FESTIVAL_TOKEN,
-    nonce,
-    festivalQuestionId,
-    signature,
-  ].join(VOTE_DATA_SEPARATOR);
+    web3.utils.utf8ToHex(
+      [
+        festivalAnswerIds.join(VOTE_DATA_SEPARATOR),
+        VOTE_DATA_FESTIVAL_TOKEN,
+        [nonce, festivalQuestionId].join(VOTE_DATA_SEPARATOR),
+      ].join(''),
+    ),
+    VOTE_DATA_SIGNATURE_TOKEN,
+    signature.slice(2),
+  ].join('');
 }
 
 export function decodeVoteData(code) {
   try {
-    const codeParts = code.split(VOTE_DATA_FESTIVAL_TOKEN);
+    if (!code) {
+      throw new Error('No code given');
+    }
 
-    const festivalAnswerIds = codeParts[0].split(VOTE_DATA_SEPARATOR);
+    if (!web3.utils.isHexStrict(code)) {
+      throw new Error('Invalid code');
+    }
 
-    const [nonce, festivalQuestionId, signature] = codeParts[1].split(
-      VOTE_DATA_SEPARATOR,
-    );
+    const parts = code.toLowerCase().split(VOTE_DATA_SIGNATURE_TOKEN);
+    const meta = web3.utils.hexToUtf8(parts[0]);
+    const metaParts = meta.split(VOTE_DATA_FESTIVAL_TOKEN);
+
+    const festivalAnswerIds = metaParts[0]
+      .split(VOTE_DATA_SEPARATOR)
+      .map((value) => {
+        if (isNaN(value)) {
+          throw new Error('Invalid festivalAnswerId');
+        }
+
+        return parseInt(value, 10);
+      });
+
+    const [nonce, festivalQuestionId] = metaParts[1]
+      .split(VOTE_DATA_SEPARATOR)
+      .map((value) => {
+        return parseInt(value, 10);
+      });
+
+    const signature = `0x${parts[1]}`;
 
     if (isNaN(nonce)) {
       throw new Error('Invalid nonce');
@@ -74,7 +101,7 @@ export function decodeVoteData(code) {
       nonce,
       signature,
     };
-  } catch {
+  } catch (error) {
     throw new Error('Could not decode vote data');
   }
 }
