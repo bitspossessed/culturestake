8da8b2cb10ec7ad2b7159f5ffed1bb9544068c27
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-29 12:51:36 +0200

    Fix chainId migration, address was already removed

--- src/server/database/migrations/20200721170447-add-chain-id-to-questions.js
+++ src/server/database/migrations/20200721170447-add-chain-id-to-questions.js
@@ -2,19 +2,27 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface
       .addColumn('questions', 'chainId', {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(66),
+        unique: true,
       })
-      .then(() => {
-        queryInterface.removeColumn('questions', 'address');
-      });
+      .then(
+        queryInterface.sequelize.query(`
+          UPDATE questions
+          SET "chainId" =
+            cast((SELECT floor(random() * 10000) + 1 WHERE questions.id = questions.id)
+            AS text);`),
+      )
+      .then(
+        queryInterface.changeColumn('questions', 'chainId', {
+          type: Sequelize.STRING(66),
+          unique: true,
+          allowNull: false,
+        }),
+      );
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface
-      .addColumn('questions', 'address', {
-        type: Sequelize.STRING,
-      })
-      .then(() => {
-        queryInterface.removeColumn('questions', 'chainId');
-      });
+    return queryInterface.addColumn('questions', 'chainId', {
+      type: Sequelize.STRING,
+    });
   },
 };
