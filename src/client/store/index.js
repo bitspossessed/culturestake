51c001aab8edc078d70855d5263629a7f7c38d5d
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-29 19:33:41 +0200

    Refactor: Remove resources hooks and use requests instead

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -5,7 +5,6 @@ import app from '~/client/store/app/reducers';
 import booth from '~/client/store/booth/reducers';
 import ethereum from '~/client/store/ethereum/reducers';
 import notifications from '~/client/store/notifications/reducers';
-import resources from '~/client/store/resources/reducers';
 import tables from '~/client/store/tables/reducers';
 import vote from '~/client/store/vote/reducers';
 
@@ -15,7 +14,6 @@ const rootReducer = combineReducers({
   booth,
   ethereum,
   notifications,
-  resources,
   tables,
   vote,
 });
