ea573b95cae77c62dc01c673b6df38dd29ebe06a
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-07-30 18:34:35 +0200

    Remove console.log

--- src/server/middlewares/fetchFromGraph.js
+++ src/server/middlewares/fetchFromGraph.js
@@ -4,7 +4,6 @@ export default function fetchFromGraphMiddleware(queryBuilder, params) {
   return async (req, res, next) => {
     const query = queryBuilder({ ...params });
     const graphData = await requestGraph(query);
-    console.log(graphData)
 
     req.locals = req.locals || {};
     req.locals.graphData = graphData;
