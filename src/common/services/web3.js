ac5c26f14c04bf92ed5f5635e5b3e39e0752457d
Andreas Dzialocha <x12@adz.garden>
2020-08-17 19:34:41 +0200

    Use http endpoint for ethereum node

--- src/common/services/web3.js
+++ src/common/services/web3.js
@@ -1,11 +1,11 @@
 import Web3 from 'web3';
 
-const rpcUrl = process.env.ETHEREUM_NODE_ENDPOINT_WS;
+const rpcUrl = process.env.ETHEREUM_NODE_ENDPOINT;
 
 const provider =
   typeof window !== 'undefined' && window.ethereum
     ? window.ethereum
-    : new Web3.providers.WebsocketProvider(rpcUrl);
+    : new Web3.providers.HttpProvider(rpcUrl);
 
 const web3 = new Web3(provider);
 
