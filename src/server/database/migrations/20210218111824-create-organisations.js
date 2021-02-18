947ef3ac6d0aecfeb653c10be0ca7bcbebe3dcd6
aeluteia <aeluteia@riseup.net>
2021-02-18 12:23:07 +0100

    Add create organisations migration

---
+++ src/server/database/migrations/20210218111824-create-organisations.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('organisations', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+      slug: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING(128),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('organisations');
+  },
+};
+
