5719c0e5551adcce5fab0242732480abf0e7eab1
aeluteia <aeluteia@riseup.net>
2021-02-19 16:30:35 +0100

    Add validations for organisations

---
+++ src/server/validations/organisations.js
@@ -0,0 +1,48 @@
+import { Joi, Segments } from 'celebrate';
+
+import { imagesValidation } from '~/common/helpers/validate';
+import {
+  paginationValidation,
+  queryValidation,
+  slugValidation,
+} from '~/server/validations';
+
+const defaultValidation = {
+  description: Joi.string().max(2000).required(),
+  images: imagesValidation.max(3),
+  name: Joi.string().max(128).required(),
+  url: Joi.string().uri().allow(''),
+};
+
+export default {
+  create: {
+    [Segments.BODY]: {
+      ...defaultValidation,
+    },
+  },
+  readAll: {
+    [Segments.PARAMS]: {
+      ...paginationValidation,
+      ...queryValidation,
+      orderKey: Joi.string().valid('id', 'createdAt', 'updatedAt', 'name'),
+    },
+  },
+  read: {
+    [Segments.PARAMS]: {
+      ...slugValidation,
+    },
+  },
+  update: {
+    [Segments.PARAMS]: {
+      ...slugValidation,
+    },
+    [Segments.BODY]: {
+      ...defaultValidation,
+    },
+  },
+  destroy: {
+    [Segments.PARAMS]: {
+      ...slugValidation,
+    },
+  },
+};
