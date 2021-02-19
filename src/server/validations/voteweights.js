88436e3d91f776e5d38a2e91d8b2cf148e4c8949
ana0 <sarah.anne.friend@gmail.com>
2021-02-19 12:00:59 +0100

    Use web3 validators for hotspot

--- src/server/validations/voteweights.js
+++ src/server/validations/voteweights.js
@@ -1,19 +1,24 @@
 import { Joi, Segments } from 'celebrate';
 
 import { idValidation, paginationValidation } from '~/server/validations';
-import { VOTEWEIGHT_TYPES } from '~/server/models/voteweight'
+import { VOTEWEIGHT_TYPES } from '~/server/models/voteweight';
+import { web3Validators } from '~/common/helpers/validate';
 
 const defaultValidation = {
   festivalId: Joi.number().integer().required(),
   strength: Joi.number().integer().required(),
-  type: Joi.string().valid(...VOTEWEIGHT_TYPES).required(),
-  location: Joi.object().keys({
-    latitude: Joi.number().min(-90).max(90),
-    longitude: Joi.number().min(-180).max(180),
-  }).allow(null),
+  type: Joi.string()
+    .valid(...VOTEWEIGHT_TYPES)
+    .required(),
+  location: Joi.object()
+    .keys({
+      latitude: Joi.number().min(-90).max(90),
+      longitude: Joi.number().min(-180).max(180),
+    })
+    .allow(null),
   radius: Joi.number().allow(null),
   organisationId: Joi.number().integer().allow(null),
-  hotspot: Joi.string().max(42).allow(null),
+  hotspot: web3Validators.web3().address().allow(null),
 };
 
 export default {
