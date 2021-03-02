1b7b3a4dc3b347c6578df8520eaa09da6bdf097f
aeluteia <aeluteia@riseup.net>
2021-03-02 16:46:38 +0100

    Add online field to festivals controller

--- src/server/controllers/festivals.js
+++ src/server/controllers/festivals.js
@@ -33,7 +33,7 @@ import { respondWithSuccess } from '~/server/helpers/respond';
 
 const options = {
   model: Festival,
-  fields: [...festivalFields, 'images', 'artworks', 'voteweights'],
+  fields: [...festivalFields, 'images', 'artworks', 'online', 'voteweights'],
   fieldsProtected: ['documents', 'chainId'],
   include: [
     FestivalBelongsToManyArtworks,
