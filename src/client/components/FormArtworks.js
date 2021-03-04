19601830c3a1e3aeeef75d93ca11e6f4bc43308f
aeluteia <aeluteia@riseup.net>
2021-03-04 13:09:30 +0100

    Move imageCredits to alphabetise object properties

--- src/client/components/FormArtworks.js
+++ src/client/components/FormArtworks.js
@@ -21,12 +21,12 @@ const FormArtworks = () => {
     description: Joi.string().max(2000).required(),
     descriptionCommission: Joi.string().max(2000).required(),
     documents: documentsValidation.required().max(3),
+    imageCredits: Joi.string().max(500).allow(''),
     images: imagesValidation.required().max(10),
     sticker: stickerValidation.required(),
     subtitle: Joi.string().max(255).required(),
     title: Joi.string().max(128).required(),
     url: Joi.string().uri().allow(''),
-    imageCredits: Joi.string().max(500).allow(''),
   };
 
   return (
