8f27c2ff1599c07b377e603ddae90d5fc0edc2ed
ana0 <sarah.anne.friend@gmail.com>
2021-04-02 19:14:20 +0200

    Begin hotspot component

--- src/server/controllers/hotspot.js
+++ src/server/controllers/hotspot.js
@@ -2,19 +2,32 @@ import httpStatus from 'http-status';
 
 import { respondWithError, respondWithSuccess } from '~/server/helpers/respond';
 import { signBooth } from '~/common/services/vote';
+import Answers from '~/server/models/answer';
 
 async function read(req, res) {
   const hotspotAddress = process.env.HOTSPOT_ADDRESS;
   const hotspotPrivKey = process.env.HOTSPOT_PRIV_KEY;
-  const hotspotFestivalQuestion = process.env.HOTSPOT_FESTIVAL;
-  //const hotspotMaxVotes = process.env.HOTSPOT_MAX_VOTES;
+  const hotspotFestivalQuestion = process.env.HOTSPOT_FESTIVAL_QUESTION;
+  const hotspotMaxVotes = process.env.HOTSPOT_MAX_VOTES;
+  const nonce = 0;
 
   if (!hotspotPrivKey || !hotspotAddress || !hotspotFestivalQuestion) {
     respondWithError(res, { message: 'Unauthorized' }, httpStatus.UNAUTHORIZED);
   }
 
-  const nonce = 0;
-  const festivalAnswerIds = [];
+  if (nonce > hotspotMaxVotes) {
+    respondWithError(
+      res,
+      { message: 'Unprocessable' },
+      httpStatus.UNPROCESSABLE_ENTITY,
+    );
+  }
+
+  const answers = await Answers.find({
+    where: { questionId: hotspotFestivalQuestion },
+  });
+
+  const festivalAnswerIds = answers.map((answer) => answer.id);
 
   const voteData = {
     booth: hotspotAddress,
